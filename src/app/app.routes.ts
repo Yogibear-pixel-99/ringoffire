a989c913b4c2e7b688a36481f3636411e3e9fc13
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-06-20 12:18:05 +0200

    added start game route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
 export const routes: Routes = [
-    { path : '', component : StartScreenComponent}
+    { path : '', component : StartScreenComponent},
+    { path : 'game', component : GameScreenComponent},
 ];
--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+
+  constructor(private router: Router){ }
+
+  newGame(){
+    this.router.navigate(["/game"]);
+  }
 
 }
